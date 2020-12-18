e6688ea2bbb0c26107f16a87c304f13a12a7cceb
kicok <kicoker@gmail.com>
2020-12-18 17:28:46 +0900

    acess React.refs 2 - addEventListener(..)

--- src/commponents/ImageCard.js
+++ src/commponents/ImageCard.js
@@ -7,9 +7,15 @@ class ImageCard extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.imageRef);
-    console.log(this.imageRef.current.clientHeight); // 왜 clientHeight를 제대로 가져오지 못하고 0으로만 찍힐까?
+    //console.log(this.imageRef);
+    // 왜 clientHeight를 제대로 가져오지 못하고 0으로만 찍힐까?
+    // 이미지가 로드 되지 않았기 때문에..
+    //console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener('load', this.setSpans);
   }
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight); //load 완료후 이미지 clientHeight 를 정상적으로 가져온다.
+  };
   render() {
     const { description, urls } = this.props.image;
     return (
